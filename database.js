815ef5b6b343307a8204c68b40727b4d9d64d2da
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2025-05-29 15:55:23 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,15 +2,14 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const con = mysql.createPool({
-    host: 'localhost' || process.env.DB_HOST,
-    user: 'root' || process.env.DB_USER,
-    password: '' || process.env.DB_PASS,
-    database: 'nirav' || process.env.DB_NAME,
+    host:  process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
-
 con.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to database:', err.message);
