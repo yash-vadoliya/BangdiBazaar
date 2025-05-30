3bb1fc150984ce10f14b51d69903cf49a851a9a8
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2025-05-30 17:18:21 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,19 +1,8 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const dns = require('dns');
-
-dns.lookup(process.env.DB_HOST, (err, address, family) => {
-  if (err) {
-    console.error('DNS lookup failed:', err);
-  } else {
-    console.log(`Address: ${address}, Family: IPv${family}`);
-  }
-});
-
-
-const con = mysql.createPool({
-    host: process.env.DB_HOST,
+const connection = mysql.createConnection({
+    host:  process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
@@ -21,13 +10,13 @@ const con = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
-con.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error connecting to database:', err.message);
-    } else {
-        console.log('Database is Connect Successfully..');
-        // connection.release();
-    }
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    return;
+  }
+  console.log('Connected to MySQL!');
 });
 
-module.exports = con;
+module.exports = connection;
