48646e20e70457ee7e744f6eb2af942f0ac5c939
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2025-06-06 11:00:55 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,28 +1,28 @@
-// const mysql = require('mysql');
-// require('dotenv').config();
+const mysql = require('mysql');
+require('dotenv').config();
 
-// const connection = mysql.createConnection({
-//     host:  process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
+const connection = mysql.createConnection({
+    host:  process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL:', err.message);
-//     return;
-//   }
-//   console.log('Connected to MySQL!');
-// });
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    return;
+  }
+  console.log('Connected to MySQL!');
+});
 
-// module.exports = connection;
-import postgres from 'postgres'
+module.exports = connection;
+// import postgres from 'postgres'
 
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+// const connectionString = process.env.DATABASE_URL
+// const sql = postgres(connectionString)
 
-export default sql
+// export default sql
